6e86de098e18d1877ffcac8bce82e5a71e59dfc0
Danelle Pontzious <danellepontzious@Unknown.domain>
2019-02-25 21:57:30 -0700

    friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,31 @@
 var friendsData = [
     {
-        name: "Danelle",
-        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fi8nLrvcNcrg%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Di8nLrvcNcrg&docid=8BohrDA0JdrFWM&tbnid=12qShbrHESfvDM%3A&vet=10ahUKEwjYz_ul5dDgAhW0OH0KHQgUDBcQMwhtKAEwAQ..i&w=1280&h=720&bih=679&biw=1042&q=cat&ved=0ahUKEwjYz_ul5dDgAhW0OH0KHQgUDBcQMwhtKAEwAQ&iact=mrc&uact=8",
-        scores: ["1", "5", "2", "5", "5"]
+        name: "Ron",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/RonSwanson.jpg/250px-RonSwanson.jpg",
+        scores: ["1", "5", "2", "5", "5", "3", "4", "4", "5", "1"]
     },
     {
-        name: "Robbie",
-        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fi8nLrvcNcrg%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Di8nLrvcNcrg&docid=8BohrDA0JdrFWM&tbnid=12qShbrHESfvDM%3A&vet=10ahUKEwjYz_ul5dDgAhW0OH0KHQgUDBcQMwhtKAEwAQ..i&w=1280&h=720&bih=679&biw=1042&q=cat&ved=0ahUKEwjYz_ul5dDgAhW0OH0KHQgUDBcQMwhtKAEwAQ&iact=mrc&uact=8",
-        scores: ["5", "3", "2", "3", "1"]
+        name: "Anne",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwhkDB7huN67Lx_vPikM16pPr8qJTz8oIu_lsWJDJk4l_RcIsB",
+        scores: ["1", "4", "2", "2", "5", "3", "3", "4", "5", "1"]
+
+    },
+    {
+        name: "Leslie",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfgWVUjtNgU2RIpJWsln3XzlmX92Hi3FeGRPsw2x7Kot2LP67hJA",
+        scores: ["4", "4", "1", "5", "5", "5", "5", "4", "5", "1"]
+
+    },
+    {
+        name: "Ben",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GQ5p4S9Sh6-_i9a8b3rK4jP_QVUg3XNtfL7c5o1MUm97iqCf",
+        scores: ["1", "1", "2", "2", "1", "3", "1", "4", "1", "1"]
+
+    },
+    {
+        name: "Andy",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTco-QExt3MeycSHbeKm6YySth6BDYIIZRCNMCXCiyYH-aad3x3-g",
+        scores: ["5", "5", "5", "2", "5", "1", "1", "4", "5", "1"]
 
     }];
 module.exports = friendsData;
